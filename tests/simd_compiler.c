3e2d0bd720a4416d50514d74f8f840f34f2f61f1
Charlie Malmqvist <charlie.malmqvist1@gmail.com>
2025-01-26 18:31:33 +0100

    simd compiler stuff

--- tests/simd_compiler.c
+++ tests/simd_compiler.c
@@ -14,6 +14,8 @@
 #define OSTD_NO_GRAPHICS
 #include "../src/ostd.h"
 
+#define MAX_IR_REGS 1024
+
 unit_local u64 get_next_power_of_two(u64 x) {
 	if (x == 0) {
 		return 1;
@@ -134,7 +136,7 @@ typedef struct Code_Expr {
 unit_local string source;
 unit_local File_Handle out_file;
 
-unit_local Token tokens[8196];
+unit_local Token tokens[16384];
 unit_local u64 token_count = 0;
 
 unit_local Token *decl_tokens[1024];
@@ -231,7 +233,9 @@ typedef struct IR_Instr {
 	Operand op0, op1, op2;
 } IR_Instr;
 
+struct IR_Program;
 typedef struct IR_Block {
+	struct IR_Program *prog;
 	s64 base_vgpr;
 	s64 base_sgpr;
 	
@@ -261,6 +265,12 @@ typedef struct IR_Program {
 	
 	s64 base_vgpr;
 	s64 base_sgpr;
+	
+	s64 output_regs[128];
+	s64 input_regs[128];
+	
+	u64 reg_reference_counts[MAX_IR_REGS];
+	
 	
 	IR_Block init_block;
 	IR_Block exit_block;
@@ -298,14 +308,20 @@ unit_local void put_ir_instr_0(IR_Block *block, Instr instr) {
 }
 unit_local void put_ir_instr_1(IR_Block *block, Instr instr, Operand op0) {
 	ir_block_reserve(block, block->instruction_count+1);
+	if (op0.reg >= 0 && op0.reg <= MAX_IR_REGS) block->prog->reg_reference_counts[op0.reg]++;
 	block->instructions[block->instruction_count++] = (IR_Instr) {instr, op0, noop(), noop() };
 }
 unit_local void put_ir_instr_2(IR_Block *block, Instr instr, Operand op0, Operand op1) {
 	ir_block_reserve(block, block->instruction_count+1);
+	if (op0.reg >= 0 && op0.reg <= MAX_IR_REGS) block->prog->reg_reference_counts[op0.reg]++;
+	if (op1.reg >= 0 && op1.reg <= MAX_IR_REGS) block->prog->reg_reference_counts[op1.reg]++;
 	block->instructions[block->instruction_count++] = (IR_Instr) {instr, op0, op1, noop() };
 }
 unit_local void put_ir_instr_3(IR_Block *block, Instr instr, Operand op0, Operand op1, Operand op2) {
 	ir_block_reserve(block, block->instruction_count+1);
+	if (op0.reg >= 0 && op0.reg <= MAX_IR_REGS) block->prog->reg_reference_counts[op0.reg]++;
+	if (op1.reg >= 0 && op1.reg <= MAX_IR_REGS) block->prog->reg_reference_counts[op1.reg]++;
+	if (op2.reg >= 0 && op2.reg <= MAX_IR_REGS) block->prog->reg_reference_counts[op2.reg]++;
 	block->instructions[block->instruction_count++] = (IR_Instr) {instr, op0, op1, op2 };
 }
 
@@ -326,14 +342,17 @@ unit_local void put_ir_header(IR_Program *prog) {
 			case TYPE_FLOAT32:
 				if (decl->storage_flags & STORAGE_INPUT) {
 					prog->input_size_bytes += 32;
+					prog->input_regs[prog->input_count] = prog->base_vgpr++;
 					prog->input_sizes[prog->input_count++] = 32;
 				}
 				if (decl->storage_flags & STORAGE_INPUT_SCALAR) {
 					prog->input_size_bytes += 4;
+					prog->input_regs[prog->input_count] = prog->base_vgpr++;
 					prog->input_sizes[prog->input_count++] = 4;
 				}
 				if (decl->storage_flags & STORAGE_OUTPUT) {
 					prog->output_size_bytes += 32;
+					prog->output_regs[prog->output_count] = prog->base_vgpr++;
 					prog->output_sizes[prog->output_count++] = 32;
 				}
 				break;
@@ -353,6 +372,7 @@ unit_local void put_ir_header(IR_Program *prog) {
 	prog->base_vgpr = 0;
 	
 	u64 offset = 0;
+	u64 input_index = 0;
 	for (u64 i = 0; i < decl_count; i += 1) {
 		Code_Decl *decl = &decls[i];
 		
@@ -361,13 +381,15 @@ unit_local void put_ir_header(IR_Program *prog) {
 			case TYPE_UINT32: // fallthrough
 			case TYPE_FLOAT32:
 				if (decl->storage_flags & STORAGE_INPUT_SCALAR) {
-					put_ir_instr_2(&prog->init_block, i_broadcast32_scalar, op_reg(prog->base_vgpr++), op_builtin(o_in_buffer, offset));
+					put_ir_instr_2(&prog->init_block, i_broadcast32_scalar, op_reg(prog->input_regs[input_index]), op_builtin(o_in_buffer, offset));
 					
 					offset += 4;
+					input_index += 1;
 				}
 				if (decl->storage_flags & STORAGE_INPUT) {
-					put_ir_instr_2(&prog->init_block, i_movu, op_reg(prog->base_vgpr++), op_builtin(o_in_buffer, offset));
+					put_ir_instr_2(&prog->init_block, i_movu, op_reg(prog->input_regs[input_index]), op_builtin(o_in_buffer, offset));
 					offset += 32;
+					input_index += 1;
 				}
 				break;
 			
@@ -480,7 +502,7 @@ unit_local void put_ir_expr(IR_Block *block, struct Code_Expr *expr)
 
 	case EXPR_LITERAL_FLOAT:
 	case EXPR_LITERAL_INT:
-		expr->read_reg = 8196+expr->imm_index;
+		expr->read_reg = MAX_IR_REGS+expr->imm_index;
 		break;
 	case EXPR_TYPE:
 		break;
@@ -502,7 +524,7 @@ unit_local void put_ir_footer(IR_Program *prog) {
 			case TYPE_UINT32: // fallthrough
 			case TYPE_FLOAT32:
 				if (decl->storage_flags & STORAGE_OUTPUT) {
-					put_ir_instr_2(&prog->exit_block, i_mov, op_builtin(o_out_buffer, offset), op_reg(r+2));
+					put_ir_instr_2(&prog->exit_block, i_mov, op_builtin(o_out_buffer, offset), op_reg(prog->output_regs[r]));
 					r += 1;
 					offset += 32;
 				}
@@ -514,21 +536,149 @@ unit_local void put_ir_footer(IR_Program *prog) {
 	}
 }
 
-
+typedef struct Text_X64_Reg_State {
+	s64 ir_reg_current;
+	bool locked;
+} Text_X64_Reg_State;
 typedef struct Text_X64_Emitter {
 	IR_Program *ir;
 	File_Handle file;
+	
+	Text_X64_Reg_State regs[15];
+	s64 ir_reg_stack[MAX_IR_REGS];
+	u64 ir_reg_stack_offsets[MAX_IR_REGS];
+	u64 ir_reg_stack_count;
+	
+	u64 reg_reference_counts[MAX_IR_REGS];
+	
+	u64 stack_position;
 } Text_X64_Emitter;
 
-unit_local string text_x64_format_op(Operand op, s64 i) {
+unit_local void text_x64_lock_reg(Text_X64_Emitter *x64, s64 x64_reg) {
+	x64->regs[x64_reg].locked = true;
+}
+unit_local void text_x64_unlock_reg(Text_X64_Emitter *x64, s64 x64_reg) {
+	x64->regs[x64_reg].locked = false;
+}
+
+unit_local s64 text_x64_get_or_allocate_reg(Text_X64_Emitter *x64, s64 ir_reg, Text_X64_Reg_State **result) {
+	assert(ir_reg > -1);
+	u64 reg_count = sizeof(x64->regs)/sizeof(x64->regs[0]);
+	
+	// First, see if the ir reg is loaded in an actual reg
+	for (u64 i = 0; i < reg_count; i += 1) {
+		if (x64->regs[i].ir_reg_current == ir_reg) {
+			if (result) *result = &x64->regs[ir_reg];
+			return (s64)i;
+		}
+	}
+	
+	// Then, see if the ir_reg is currently spilled to the stack, and in that case swap a
+	// reg value with the spilled stack value
+	for (u64 j = 0; j < x64->ir_reg_stack_count; j += 1) {
+		if (x64->ir_reg_stack[j] == ir_reg) {
+			for (u64 i = 0; i < reg_count; i += 1) {
+				Text_X64_Reg_State *reg = &x64->regs[i];
+				if (reg->locked) continue;
+				
+				if (reg->ir_reg_current == -1) {
+					fprint(x64->file, "\n; stack $%u -> ymm%u\n", ir_reg, i);
+					if (x64->ir_reg_stack_offsets[j]) 
+						fprint(x64->file, "add rbx, %u\n", x64->ir_reg_stack_offsets[j]);
+					fprint(x64->file, "vmovdqu ymm%u, ymmword ptr [rbx]\n", i);
+					if (x64->ir_reg_stack_offsets[j]) 
+						fprint(x64->file, "sub rbx, %u\n", x64->ir_reg_stack_offsets[j]);
+					return (s64)i;
+				} else {
+					fprint(x64->file, "\n; stack $%u -> ymm%u | $%u -> stack \n", ir_reg, i, reg->ir_reg_current);
+					if (x64->ir_reg_stack_offsets[j]) 
+						fprint(x64->file, "add rbx, %u\n", x64->ir_reg_stack_offsets[j]);
+					fprint(x64->file, "vmovdqu ymm15, ymmword ptr [rbx]\n");
+					fprint(x64->file, "vmovdqu ymmword ptr [rbx], ymm%u\n", i);
+					if (x64->ir_reg_stack_offsets[j]) 
+						fprint(x64->file, "sub rbx, %u\n", x64->ir_reg_stack_offsets[j]);
+					fprint(x64->file, "\n");
+					
+					Swap(reg->ir_reg_current, x64->ir_reg_stack[j]);
+					if (result) *result = &x64->regs[i];
+					reg->ir_reg_current = ir_reg;
+					return (s64)i;
+				}
+			}
+		}
+	}
+	
+	// Lastly, spill a reg to the stack
+	s64 reg_index = -1;
+	for (u64 i = 0; i < reg_count; i += 1) {
+		if (x64->regs[i].ir_reg_current == -1) {
+			reg_index = (s64)i;
+			break;
+		}
+	}
+	
+	if (reg_index == -1) {
+		for (u64 i = 0; i < reg_count; i += 1) {
+			if (x64->regs[i].locked == false) {
+				reg_index = (s64)i;
+				break;
+			}
+		}
+	}
+	
+	assert(reg_index != -1);
+	
+	Text_X64_Reg_State *reg = &x64->regs[reg_index];
+	
+	if (reg->ir_reg_current != -1) {
+		x64->ir_reg_stack_offsets[x64->ir_reg_stack_count] = x64->stack_position;
+		x64->ir_reg_stack[x64->ir_reg_stack_count++] = reg->ir_reg_current;
+		fprint(x64->file, "\n; ymm%u $%u -> stack | $%u -> ymm%u\n", reg_index, reg->ir_reg_current, ir_reg, reg_index);
+		fprint(x64->file, "add rbx, %u\n", x64->stack_position);
+		fprint(x64->file, "vmovdqu ymmword ptr [rbx], ymm%u\n", reg_index);
+		fprint(x64->file, "sub rbx, %u\n\n", x64->stack_position);
+		x64->stack_position += 32;
+	}
+	
+	reg->ir_reg_current = ir_reg;
+	if (result) *result = &x64->regs[reg_index];
+	return reg_index;
+}
+
+unit_local void text_x64_free_reg(Text_X64_Emitter *x64, s64 ir_reg) {
+	u64 reg_count = sizeof(x64->regs)/sizeof(x64->regs[0]);
+	// First, see if the ir reg is loaded in an actual reg, and free it
+	for (u64 i = 0; i < reg_count; i += 1) {
+		if (x64->regs[i].ir_reg_current == ir_reg) {
+			x64->regs[i].ir_reg_current = -1;
+		}
+	}
+	
+	
+	for (u64 i = 0; i < reg_count; i += 1) {
+		for (u64 j = 0; j < x64->ir_reg_stack_count; j += 1) {
+			if (x64->ir_reg_stack[j] == ir_reg) {
+				x64->ir_reg_stack[j] = -1;
+			}
+		}
+	}
+	
+	fprint(x64->file, "\n; Free $%u\n\n", ir_reg);
+}
+
+unit_local string text_x64_format_op(Text_X64_Emitter *x64, Operand op, s64 i, Text_X64_Reg_State **result) {
 	string sop = STR("");
 	switch (op.kind) {
-		case o_reg:
-			sop = (op.reg < 8196 ? tprint("ymm%u", op.reg) : tprint("i%u", i));
+		case o_reg: {
+			s64 x64_op = op.reg < MAX_IR_REGS ? text_x64_get_or_allocate_reg(x64, op.reg, result) : 0;
+			sop = (op.reg < MAX_IR_REGS ? tprint("ymm%u", x64_op) : tprint("i%u", i));
 			break;
-		case o_mem:
-			sop = tprint("[ymm%u]", op.reg);
+		}
+		case o_mem: {
+			s64 x64_op = text_x64_get_or_allocate_reg(x64, op.reg, result);
+			sop = tprint("[ymm%u]", x64_op);
 			break;
+		}
 		case o_builtin:
 			switch(op.builtin) {
 			
@@ -553,7 +703,8 @@ unit_local void emit_text_x64_block(Text_X64_Emitter *x64, IR_Block *block) {
 		fprint(x64->file, "\npush rbp\n");
 		fprint(x64->file, "mov rbp, rsp\n");
 		u64 sz = get_next_power_of_two(block->stack_size);
-		fprint(x64->file, "sub rsp, %u\n\n", sz);
+		fprint(x64->file, "sub rsp, %u\n", sz);
+		fprint(x64->file, "mov rbx, rsp\n\n");
 	}
 	
 	for (u64 i = 0; i < block->instruction_count; i += 1) {
@@ -562,12 +713,16 @@ unit_local void emit_text_x64_block(Text_X64_Emitter *x64, IR_Block *block) {
 		Operand op0 = instr.op0;
 		Operand op1 = instr.op1;
 		Operand op2 = instr.op2;
-		s64 i0 = op0.reg-8196;
-		s64 i1 = op1.reg-8196;
-		s64 i2 = op2.reg-8196;
-		string sop0 = text_x64_format_op(op0, i0);
-		string sop1 = text_x64_format_op(op1, i1);
-		string sop2 = text_x64_format_op(op2, i2);
+		s64 i0 = op0.reg-MAX_IR_REGS;
+		s64 i1 = op1.reg-MAX_IR_REGS;
+		s64 i2 = op2.reg-MAX_IR_REGS;
+		Text_X64_Reg_State *reg0 = 0, *reg1 = 0, *reg2 = 0;
+		string sop0 = text_x64_format_op(x64, op0, i0, &reg0);
+		if (reg0) reg0->locked = true;
+		string sop1 = text_x64_format_op(x64, op1, i1, &reg1);
+		if (reg1) reg1->locked = true;
+		string sop2 = text_x64_format_op(x64, op2, i2, &reg2);
+		if (reg2) reg2->locked = true;
 		string istr = instr_avx_str(instr.i);
 		
 		switch (instr.i) {
@@ -575,12 +730,14 @@ unit_local void emit_text_x64_block(Text_X64_Emitter *x64, IR_Block *block) {
 			case i_i32tof32: // fallthrough
 			case i_movu: // fallthrough
 			case i_mov:
+				fprint(x64->file, "; %s $%i, $%i\n", istr, op0.reg, op1.reg);
 				fprint(x64->file, "%s %s, %s", istr, sop0, sop1);
 				break;
 			case i_add: // fallthrough
 			case i_sub: // fallthrough
 			case i_mul: // fallthrough
 			case i_div:
+				fprint(x64->file, "; %s $%i, $%i, $%i\n", istr, op0.reg, op1.reg, op2.reg);
 				fprint(x64->file, "%s %s, %s, %s", istr, sop0, sop1, sop2);
 				break;
 			case i_broadcast32_scalar: 
@@ -597,12 +754,37 @@ unit_local void emit_text_x64_block(Text_X64_Emitter *x64, IR_Block *block) {
 			default: assert(false); break;
 		}
 		
+		if (reg0) reg0->locked = false;
+		if (reg1) reg1->locked = false;
+		if (reg2) reg2->locked = false;
+		
+		if (reg0) {
+			x64->reg_reference_counts[reg0->ir_reg_current]++;
+			if (x64->reg_reference_counts[reg0->ir_reg_current] >= x64->ir->reg_reference_counts[reg0->ir_reg_current]) {
+				text_x64_free_reg(x64, reg0->ir_reg_current);
+			}
+		}
+		if (reg1) {
+			x64->reg_reference_counts[reg1->ir_reg_current]++;
+			if (x64->reg_reference_counts[reg1->ir_reg_current] >= x64->ir->reg_reference_counts[reg1->ir_reg_current]) {
+				text_x64_free_reg(x64, reg1->ir_reg_current);
+			}
+			x64->reg_reference_counts[reg1->ir_reg_current]++;
+		}
+		if (reg2) {
+			x64->reg_reference_counts[reg2->ir_reg_current]++;
+			if (x64->reg_reference_counts[reg2->ir_reg_current] >= x64->ir->reg_reference_counts[reg2->ir_reg_current]) {
+				text_x64_free_reg(x64, reg2->ir_reg_current);
+			}
+			x64->reg_reference_counts[reg2->ir_reg_current]++;
+		}
+		
 		fprint(x64->file, "\n");
 	}
 	
 	if (block->stack_size > 0) {
-		fprint(x64->file, "\nmov rsp, rbp\n");
-		fprint(x64->file, "pop rbp\n\n");
+		//fprint(x64->file, "\nmov rsp, rbp\n");
+		//fprint(x64->file, "pop rbp\n\n");
 	}
 }
 
@@ -669,6 +851,10 @@ unit_local void emit_text_x64_footer(Text_X64_Emitter *x64) {
 	fprint(x64->file, "\n");
 	
 	emit_text_x64_block(x64, &x64->ir->exit_block);
+
+	// todo(charlie) move this to end of blocks, and restore registers before
+	fprint(x64->file, "\nmov rsp, rbp\n");
+	fprint(x64->file, "pop rbp\n\n");
 
 	fprint(x64->file, "\nGO ENDP\n");
 	fprint(x64->file, "DllMain PROC hinstDLL:QWORD, fdwReason:QWORD, lpReserved:QWORD\n");
@@ -842,7 +1028,8 @@ unit_local Code_Expr *parse_expr_rec(Token *expr_start, Token **next, Op_Kind le
 		Code_Expr *lexpr = parse_one_expr(expr_start, next, left_op);
 		Code_Expr *rexpr = parse_expr_rec(op_token+1, next, rop);
 		
-		if (!lexpr || !rexpr) return 0;
+		if (!lexpr || !rexpr) 
+			return 0;
 		
 		Code_Expr *parent_op = &exprs[expr_count++];
 		parent_op->read_reg = -1;
@@ -882,10 +1069,15 @@ unit_local Code_Expr *parse_expr(Token *expr_start, Token **next) {
 	Code_Expr *expr = 0;
 	
 	Token *op_token = expr_start+1;
+	if (op_token->kind == ';') {
+		return parse_one_expr(expr_start, next, (Op_Kind)0xffff);
+	}
+	
 	while  (op_token->kind != ';') {
 
 		Code_Expr *rexpr = parse_expr_rec(expr_start, next, (Op_Kind)0xffff);
-		if (!rexpr)  return 0;
+		if (!rexpr)  
+			return 0;
 
 		if (expr) {
 			Code_Expr *parent_op = &exprs[expr_count++];
@@ -1140,11 +1332,12 @@ int main(int argc, char **argv) {
 		if (do_parse_expr) {
 			// Expression
 			Token *next;
+			print("tok %u\n", expr_start->kind);
 			Code_Expr *expr = parse_expr(expr_start, &next);
 			if (!expr) {
 				return 1;
 			}
-			
+			print("expr %u\n", expr->kind);
 			if (lhs_decl) {
 				if (expr->kind == EXPR_OP) expr->op_result_reg = (s64)lhs_decl_index;
 				Code_Expr *decl_expr = &exprs[expr_count++];
@@ -1174,7 +1367,7 @@ int main(int argc, char **argv) {
 			
 			if (next->kind != ';') {
 				string next_str = (string) {next->length, source.data+next->source_pos};
-				log(0, "Expected ; after expression, got %s\n", next_str);
+				log(0, "Expected ; after statement-level expression, got %s\n", next_str);
 				return 1;
 			}
 		}
@@ -1197,12 +1390,16 @@ int main(int argc, char **argv) {
 	
 	
 	IR_Program ir = (IR_Program){0};
+	ir.init_block.prog = &ir;
+	ir.exit_block.prog = &ir;
+	
 	put_ir_header(&ir);
 	
 	IR_Block *main_block = &ir.blocks[ir.block_count++];
 	*main_block = (IR_Block){0};
-	main_block->stack_size = 16*32;
+	main_block->stack_size = decl_count*32;
 	main_block->base_vgpr = ir.base_vgpr;
+	main_block->prog = &ir;
 	
 	for (u64 i = 0; i < statement_count; i += 1) {
 		Code_Expr *expr = statements[i];
@@ -1214,6 +1411,9 @@ int main(int argc, char **argv) {
 	Text_X64_Emitter x64 = (Text_X64_Emitter){0};
 	x64.ir = &ir;
 	x64.file = out_file;
+	for (u64 i = 0; i < sizeof(x64.regs)/sizeof(x64.regs[0]); i += 1) {
+		x64.regs[i].ir_reg_current = -1;
+	}
 	
 	emit_text_x64_header(&x64);
 	emit_text_x64_block(&x64, main_block);
@@ -1282,26 +1482,26 @@ int main(int argc, char **argv) {
 }
 unit_local void emit_instr_1(Instr instr, Operand op0) {
 	string istr = instr_avx_str(instr);
-	s64 i0 = op0.reg-8196;
-	string sop0 = op0.kind == o_reg ? (op0.reg < 8196 ? tprint("ymm%u", op0.reg) : tprint("i%u", i0)) : tprint("[%s]", op0.addr);
+	s64 i0 = op0.reg-MAX_IR_REGS;
+	string sop0 = op0.kind == o_reg ? (op0.reg < MAX_IR_REGS ? tprint("ymm%u", op0.reg) : tprint("i%u", i0)) : tprint("[%s]", op0.addr);
 	fprint(out_file, "%s %s\n", istr, sop0);
 }
 unit_local void emit_instr_2(Instr instr, Operand op0, Operand op1) {
 	string istr = instr_avx_str(instr);
-	s64 i0 = op0.reg-8196;
-	string sop0 = op0.kind == o_reg ? (op0.reg < 8196 ? tprint("ymm%u", op0.reg) : tprint("i%u", i0)) : tprint("[%s]", op0.addr);
-	s64 i1 = op1.reg-8196;
-	string sop1 = op1.kind == o_reg ? (op1.reg < 8196 ? tprint("ymm%u", op1.reg) : tprint("i%u", i1)) : tprint("[%s]", op1.addr);
+	s64 i0 = op0.reg-MAX_IR_REGS;
+	string sop0 = op0.kind == o_reg ? (op0.reg < MAX_IR_REGS ? tprint("ymm%u", op0.reg) : tprint("i%u", i0)) : tprint("[%s]", op0.addr);
+	s64 i1 = op1.reg-MAX_IR_REGS;
+	string sop1 = op1.kind == o_reg ? (op1.reg < MAX_IR_REGS ? tprint("ymm%u", op1.reg) : tprint("i%u", i1)) : tprint("[%s]", op1.addr);
 	fprint(out_file, "%s %s, %s\n", istr, sop0, sop1);
 }
 unit_local void emit_instr_3(Instr instr, Operand op0, Operand op1, Operand op2) {
 	string istr = instr_avx_str(instr);
-	s64 i0 = op0.reg-8196;
-	string sop0 = op0.kind == o_reg ? (op0.reg < 8196 ? tprint("ymm%u", op0.reg) : tprint("i%u", i0)) : tprint("[%s]", op0.addr);
-	s64 i1 = op1.reg-8196;
-	string sop1 = op1.kind == o_reg ? (op1.reg < 8196 ? tprint("ymm%u", op1.reg) : tprint("i%u", i1)) : tprint("[%s]", op1.addr);
-	s64 i2 = op2.reg-8196;
-	string sop2 = op2.kind == o_reg ? (op2.reg < 8196 ? tprint("ymm%u", op2.reg) : tprint("i%u", i2)) : tprint("[%s]", op2.addr);
+	s64 i0 = op0.reg-MAX_IR_REGS;
+	string sop0 = op0.kind == o_reg ? (op0.reg < MAX_IR_REGS ? tprint("ymm%u", op0.reg) : tprint("i%u", i0)) : tprint("[%s]", op0.addr);
+	s64 i1 = op1.reg-MAX_IR_REGS;
+	string sop1 = op1.kind == o_reg ? (op1.reg < MAX_IR_REGS ? tprint("ymm%u", op1.reg) : tprint("i%u", i1)) : tprint("[%s]", op1.addr);
+	s64 i2 = op2.reg-MAX_IR_REGS;
+	string sop2 = op2.kind == o_reg ? (op2.reg < MAX_IR_REGS ? tprint("ymm%u", op2.reg) : tprint("i%u", i2)) : tprint("[%s]", op2.addr);
 	fprint(out_file, "%s %s, %s, %s\n", istr, sop0, sop1, sop2);
 }
 
@@ -1412,7 +1612,7 @@ unit_local void emit_expr(struct Code_Expr *expr)
 
 	case EXPR_LITERAL_FLOAT:
 	case EXPR_LITERAL_INT:
-		expr->read_reg = 8196+expr->imm_index;
+		expr->read_reg = MAX_IR_REGS+expr->imm_index;
 		break;
 	case EXPR_TYPE:
 		break;
