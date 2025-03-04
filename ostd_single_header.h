c4cbb7c4f27b92ffbb6f49fdc96e052ffd226c1a
Charlie Malmqvist <charlie.malmqvist1@gmail.com>
2025-03-04 20:35:06 +0100

    fix

--- ostd_single_header.h
+++ ostd_single_header.h
@@ -4040,7 +4040,10 @@ unit_local _Surface_State *_get_surface_state(Surface_Handle h) {
 //////
 /////////////////////////////////////////////////////
 
-
+#if (OS_FLAGS & OS_FLAG_LINUX)
+#define _GNU_SOURCE
+#define _POSIX_C_SOURCE
+#endif
 
 // todo(charlie) dynamically link & manually  define some stuff to minimize namespace bloat here
 #include <unistd.h>
